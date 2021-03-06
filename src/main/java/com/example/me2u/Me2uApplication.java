c969a85af67990e7e7c42d5df86731595fe8533b
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-03-06 12:01:25 -0800

    temporary CORS

--- src/main/java/com/example/me2u/Me2uApplication.java
+++ src/main/java/com/example/me2u/Me2uApplication.java
@@ -2,12 +2,25 @@ package com.example.me2u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Me2uApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Me2uApplication.class, args);
+    }
+
+    @Configuration
+    public class CrossOriginsConf implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("*");
+        }
     }
 
 }
