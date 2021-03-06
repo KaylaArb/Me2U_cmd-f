37907c52da4c6faa5e6a3310afe1d4f723e6d491
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-03-06 11:12:53 -0800

    first commit

---
+++ src/main/java/com/example/me2u/Me2uApplication.java
@@ -0,0 +1,13 @@
+package com.example.me2u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Me2uApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Me2uApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/me2u/Me2uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.me2u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Me2uApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
