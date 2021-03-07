eefd0c320dfc7c7713a4ee368e267123d5e4a951
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-03-06 19:25:32 -0800

    new cors file

--- src/main/java/com/example/me2u/Me2uApplication.java
+++ src/main/java/com/example/me2u/Me2uApplication.java
@@ -2,9 +2,6 @@ package com.example.me2u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Me2uApplication {
@@ -13,16 +10,16 @@ public class Me2uApplication {
         SpringApplication.run(Me2uApplication.class, args);
     }
 
-    @Configuration
-    public class CrossOriginsConf implements WebMvcConfigurer {
-
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
-                    .allowedHeaders("http://localhost:3000")
-                    .allowedMethods("*");
-        }
-    }
+//    @Configuration
+//    public class CrossOriginsConf implements WebMvcConfigurer {
+//
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**")
+//                    .allowedOrigins("http://localhost:3000/")
+//                    .allowedHeaders("http://localhost:3000/")
+//                    .allowedMethods("*");
+//        }
+//    }
 
 }
