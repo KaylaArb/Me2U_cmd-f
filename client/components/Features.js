05c0c7a266e2662abe19f771bd4f1cc1b7ac0a30
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-03-07 08:07:03 -0800

    updating photos

--- client/components/Features.js
+++ client/components/Features.js
@@ -1,22 +1,25 @@
 import styles from "../styles/Features.module.css";
-import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
+
 
 export default function Features() {
     return (
         <div className={styles.featureContainer}>
             <div className={styles.featureContent}>
                 <h2>Awesome Features</h2>
-                <Carousel infinite slidesPerPage={3} animationSpeed={4000000}
-                    autoPlay={0} stopAutoPlayOnHover className={styles.photoCara}>
-                    <img src='/rectangle29.png' />
-                    <img src='/rec42.jpg' />
-                    <img src='/rec31.png' />
-                    <img src='/rec41.png' />
-                    <img src='/rec32.png' />
-                    <img src='/rec40.png' />
-                    <img src='/rectangle27.png' />
+                <Carousel infinite slidesPerPage={3} animationSpeed={3600000}
+                    autoPlay={0} className={styles.photoCara}>
+                    <img src='/rec1.png' className={styles.caraPhoto}/>
+                    <img src='/recdemo.svg' className={styles.caraPhoto}/>
+                    <img src='/rec42.jpg' className={styles.caraPhoto}/>
+                    <img src='/rec2.png' className={styles.caraPhoto}/>
+                    <img src='/rec41.png' className={styles.caraPhoto}/>
+                    <img src='/rec3.png' className={styles.caraPhoto}/>
+                    <img src='/rec40.png' className={styles.caraPhoto}/>
                 </Carousel>
+               
+
             </div>
         </div>
 
